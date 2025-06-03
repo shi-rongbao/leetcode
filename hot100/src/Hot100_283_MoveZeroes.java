a4368dc5546892e675c23309d3ebfcef0814537e
ShiRongbao <7447664226srb@gmail.com>
2025-06-03 17:18:15 +0800

    feat: 283_移动零

---
+++ hot100/src/Hot100_283_MoveZeroes.java
@@ -0,0 +1,25 @@
+/**
+ * @author: ShiRongbao
+ * @date: 2025-06-03
+ * @description:
+ */
+public class Hot100_283_MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+
+}
